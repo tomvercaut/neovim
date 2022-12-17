d17a70e83c5ad25af401fe101208639110ef6b8f
Tom <tomvercaut@gmail.com>
2022-12-17 17:08:12 +0100

    Updated deprecated cmp_nvim_lsp.update_capabilities to cmp_nvim_lsp.default_capabilitites.

--- lua/user/config/lsp/rust.lua
+++ lua/user/config/lsp/rust.lua
@@ -19,7 +19,7 @@ if not ok then
   return
 end
 
-local capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
 lsp['rust_analyzer'].setup {
   capabilities = capabilities
 }
