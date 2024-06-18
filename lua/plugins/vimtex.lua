8153d476b0d434a99acc4688884402efce86b514
Tom Vercauteren <tomvercaut@gmail.com>
2024-06-18 09:53:34 +0200

    Updated Vimtex configuration.
    Enabled a silent compiler flag option. Don't open quickfix upon a
    warning. Don't show 'Underfull' and 'Overfull' warnings.

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,9 +1,16 @@
 --  lazy = false,
+-- vim.g.vimtex_quickfix_ignore_filters = ["*warning*",]
 return {
   "lervag/vimtex",
   config = function()
+    vim.g.vimtex_compiler_silent = 1
+    vim.g.vimtex_quickfix_open_on_warning = 0
     vim.g.vimtex_compiler_latexmk = {
       out_dir = "out",
+    }
+    vim.g.vimtex_quickfix_ignore_filters = {
+      "Underfull",
+      "Overfull",
     }
     vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
   end,
