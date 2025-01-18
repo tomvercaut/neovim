8e571ad5dcb29b60b8af46d83a3e7037f20150d0
Tom Vercauteren <tomvercaut@gmail.com>
2025-01-18 22:15:00 +0100

    Config update

--- init.lua
+++ init.lua
@@ -2,8 +2,8 @@
 vim.api.nvim_exec("language en_US", true)
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-require("lspconfig").typst_lsp.setup({
-  settings = {
-    exportPdf = "never",
-  },
-})
+-- require("lspconfig").typst_lsp.setup({
+--   settings = {
+--     exportPdf = "never",
+--   },
+-- })
